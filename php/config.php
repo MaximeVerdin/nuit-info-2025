34e0f82ad8c27adcc0a20f3b6e73898406694e96
Hembert <hembertdorian@gmail.com>
2025-12-04 23:06:56 +0100

    changement pour ajouter .env

--- php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database configuration
-define('DB_HOST', 'database');
-define('DB_NAME', 'nuit-info-trc-2025');
-define('DB_USER', 'nuit-info-trc-2025');
-define('DB_PASS', 'sGNvCVsSjdnp');
-?>
