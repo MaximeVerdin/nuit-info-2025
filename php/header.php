42ca2b93402b0fadea76372da361fe31d630b812
Hembert <hembertdorian@gmail.com>
2025-12-05 01:38:21 +0100

    corrections effectués et ajout du css au QCM

--- php/header.php
+++ php/header.php
@@ -5,6 +5,9 @@
     <meta name="generator" content="pandoc">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <link rel="icon" type="image/x-icon" href="https://nird.forge.apps.education.fr/favicon.ico">
+    <link rel="stylesheet" href="../css/CSSRETRO.css">
+    <link rel="stylesheet" href="../css/gamingcss.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>QCM - La Démarche NIRD</title>
     <style>
         code{white-space: pre-wrap;}
@@ -26,7 +29,7 @@
 <body>
     <header>
         <h1>
-            <a href="https://nird.forge.apps.education.fr/">
+            <a href="../index.html">
                 <img src="../index_files/logo+text206px.png" alt="Logo NIRD">
             </a>
             <span>La démarche NIRD</span>
@@ -39,6 +42,6 @@
             <img src="../index_files/navbarico.svg" style="width:30px !important; height:30px !important;">
         </label>
         <nav class="navbar nav-menu loaded" id="site-nav">
-            <p><a href="https://nird.forge.apps.education.fr/index.html">🐝 Accueil</a> <a href="https://nird.forge.apps.education.fr/demarche/">🚸 Démarche</a> <a href="https://nird.forge.apps.education.fr/pilotes/">🏫 Pilotes</a> <a href="https://nird.forge.apps.education.fr/linux/">🐧 Linux</a> <a href="https://nird.forge.apps.education.fr/tools/">🧰 Boite à outils</a> <a href="https://nird.forge.apps.education.fr/reconditionnement/">♻️ Reconditionnement</a> <a href="https://nird.forge.apps.education.fr/collectivites/">🏛️ Collectivités</a> <a href="https://nird.forge.apps.education.fr/pourquoi/">❓ Pourquoi</a> <a href="php/QCM.php">📝 QCM</a></p>
+            <p><a href="../index.html">🐝 Accueil</a> <a href="../html/demarche.html">🚸 Démarche</a> <a href="../html/pilotes.html">🏫 Pilotes</a> <a href="../html/linux.html">🐧 Linux</a> <a href="../html/tools.html">🧰 Boite à outils</a> <a href="../html/reconditionnement.html">♻️ Reconditionnement</a> <a href="../html/collectivites.html">🏛️ Collectivités</a> <a href="../html/pourquoi.html">❓ Pourquoi</a> <a href="php/QCM.php">📝 QCM</a></p>
         </nav>
     </div>
